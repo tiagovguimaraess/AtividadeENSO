0eca36550ec7e316fe392b5778f37ade62519dd3
Alex Custódio <alex.scustod@gmail.com>
2022-09-05 17:27:30 +0000

    adicionar-divisao

---
+++ api/src/main/java/app/Divisao.java
@@ -0,0 +1,19 @@
+package app;
+
+import io.jooby.annotations.*;
+import io.jooby.exception.BadRequestException;
+
+@Path("/divisao/{d1}/{d2}")
+public class Divisao {
+    @GET
+    public double dividir(@PathParam("d1") String d1, @PathParam("d2") String d2){
+        
+        try {
+            double n1 = Double.parseDouble(d1);
+            double n2 = Double.parseDouble(d2);
+            return n1/n2;
+        } catch (NumberFormatException nfe) {
+            throw new BadRequestException("Operador inválido.");
+        }
+    } 
+}
