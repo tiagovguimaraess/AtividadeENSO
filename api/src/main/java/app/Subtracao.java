e20026eb23128bfe7da428a53b11daa82b286d1e
Adelson Júnior <junioradelsonst@gmail.com>
2022-09-12 18:05:58 +0000

    subtracao

---
+++ api/src/main/java/app/Subtracao.java
@@ -0,0 +1,30 @@
+package app;
+
+import io.jooby.annotations.*;
+import io.jooby.exception.BadRequestException;
+
+@Path("/subtracao/{n1}/{n2}")
+
+public class Subtracao {
+
+    @GET
+    public double calcular(@PathParam("n1") String n1Str, @PathParam("n2") String n2Str){
+
+        try{
+
+            double n1 = Double.parseDouble(n1Str);
+            double n2 = Double.parseDouble(n2Str);
+
+            double resultado = n1 - n2;
+
+            return resultado;
+
+        } catch (NumberFormatException nfe){
+
+            throw new BadRequestException("Operador inválido.");
+
+        }
+
+    }
+
+}
