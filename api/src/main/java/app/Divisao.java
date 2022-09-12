1270a303f6710db959df2ca0c05898f598e4f32b
Alex Custódio <alex.scustod@gmail.com>
2022-09-12 18:33:50 +0000

    adicionei tratamento para 0

--- api/src/main/java/app/Divisao.java
+++ api/src/main/java/app/Divisao.java
@@ -9,8 +9,12 @@ public class Divisao {
     public double dividir(@PathParam("d1") String d1, @PathParam("d2") String d2){
 
         try {
+            
             double n1 = Double.parseDouble(d1);
             double n2 = Double.parseDouble(d2);
+            if (n2 ==0){
+                throw new BadRequestException("O segundo valor não pode ser zero");
+            }
             return n1/n2;
         } catch (NumberFormatException nfe) {
             throw new BadRequestException("Operador inválido.");
