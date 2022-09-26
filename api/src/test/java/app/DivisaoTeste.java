c780d6aa3403667c0cb3b9af9d0d9d4e53b5ea60
Alex Custódio <alex.scustod@gmail.com>
2022-09-26 17:14:06 +0000

    Adicionei alguns testes unitário

---
+++ api/src/test/java/app/DivisaoTeste.java
@@ -0,0 +1,26 @@
+package app;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import io.jooby.exception.BadRequestException;
+
+public class DivisaoTeste {
+    Divisao divisao = new Divisao();
+    
+    @Test
+    public void verificaCasoBase() {
+        double resultado = divisao.dividir("15", "3");
+        assertEquals(5.0, resultado);
+    }
+
+    @Test
+    public void verificaCasoErro() {
+        assertThrows(BadRequestException.class, ()->{
+            divisao.dividir("1", "0");
+        });
+    }
+
+}
+
