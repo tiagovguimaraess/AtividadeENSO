fbdc4a43c7302606fd7b53554bc2f550e20cd969
Alex Custódio <alex.scustod@gmail.com>
2022-09-26 17:22:03 +0000

    Adicionando BeforeEach pra ficar topado

--- api/src/test/java/app/DivisaoTeste.java
+++ api/src/test/java/app/DivisaoTeste.java
@@ -2,13 +2,18 @@ package app;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import io.jooby.exception.BadRequestException;
 
 public class DivisaoTeste {
-    Divisao divisao = new Divisao();
+    Divisao divisao;
     
+    @BeforeEach
+    public void antesCadaTeste(){
+        divisao = new Divisao();
+    }
     @Test
     public void verificaCasoBase() {
         double resultado = divisao.dividir("15", "3");
