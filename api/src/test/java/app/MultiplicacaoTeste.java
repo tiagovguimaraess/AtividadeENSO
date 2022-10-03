53184b1ca034a4a33412c27fb3b180a2bccb0d71
Carlos Eduardo Ferreira <cefs2@aluno.ifal.edu.br>
2022-10-03 17:11:51 +0000

    dupliquei

---
+++ api/src/test/java/app/MultiplicacaoTeste.java
@@ -0,0 +1,27 @@
+package app;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import io.jooby.exception.BadRequestException;
+
+public class MultiplicacaoTeste {
+    Multiplicacao multiplicacao;
+    
+    @BeforeEach
+    public void antesCadaTeste(){
+        multiplicacao = new Multiplicacao();
+    }
+    @Test
+    public void verificaCasoBase() {
+        double resultado = multiplicacao.multiplicar("15", "3");
+        assertEquals(45.0, resultado);
+    }
+    @Test
+    public void verificaCasoBase2() {
+        double resultado = multiplicacao.multiplicar("16", "4");
+        assertEquals(64.0, resultado);
+    }
+}
